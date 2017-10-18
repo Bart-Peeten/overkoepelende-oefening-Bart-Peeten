ac5b5e17be9596edf2ce9ae4322883936ef8d1c2
peetenbart <bart.peeten@gmail.com>
2017-10-18 21:24:25 +0200

    Huiswerk2

--- oef3.php
+++ oef3.php
@@ -9,6 +9,31 @@
 
 <form action="oef3verwerk.php" method="get">
   bookId:
+    <select style="width: 100px">
+        <?php
+        $user     = 'root';
+        $password = 'root';
+        $database = 'examen';
+        $pdo      = null;
+        try{
+            $pdo = new \PDO("mysql:host=localhost; dbname=$database", $user, $password);
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE,
+                \PDO::ERRMODE_EXCEPTION);
+            $statement = $pdo->query('SELECT * FROM book');
+            $statement->setFetchMode(\PDO::ATTR_ERRMODE);
+            $dbArray = $statement->fetchAll();
+        } catch (\PDOException $ex){
+            print 'Exception!: ' . $ex->getMessage();
+        }
+        if($dbArray != null)
+        {
+            foreach ($dbArray as $book)
+            {
+                echo "<option value='" .$book['id'] ."' >" . $book['id'] ." ". $book['title'] ."</option>";
+            }
+        }
+        ?>
+    </select>
 <input type="submit" value="delete"/>
 </form>
 </body>
--- oef3verwerk.php
+++ oef3verwerk.php
@@ -7,3 +7,19 @@
 <body>
 </body>
 </html>
+
+<?php
+$dbPw = 'root';
+$dbUser = "root";
+$dbServer = "localhost";
+$dbName = "examen";
+$bookID = $_POST['selection'];
+try {
+    $pdo = new PDO("mysql:host=localhost;dbname=$dbName",$dbUser,$dbPw);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $statement = $pdo->prepare('DELETE FROM book WHERE id = :id');
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    $statement->bindParam(':id', $bookID, PDO::PARAM_INT);
+    $statement->execute();
+} catch (Exception $ex) {
+}
